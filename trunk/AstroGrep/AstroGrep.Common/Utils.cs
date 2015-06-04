6bfaf1370dbe2577849835d17058caeb6eaee75f
jackslade <jackslade@15d7e090-df39-0410-93f7-b78f46d19391>
2015-06-04 22:01:30 +0000

    Add remarks/copyright comment section

--- trunk/AstroGrep/AstroGrep.Common/Utils.cs
+++ trunk/AstroGrep/AstroGrep.Common/Utils.cs
@@ -6,6 +6,33 @@ using System.Threading.Tasks;
 
 namespace AstroGrep.Common
 {
+   /// <summary>
+   /// Contains common application related utility methods.
+   /// </summary>
+   /// <remarks>
+   ///   AstroGrep File Searching Utility. Written by Theodore L. Ward
+   ///   Copyright (C) 2002 AstroComma Incorporated.
+   ///   
+   ///   This program is free software; you can redistribute it and/or
+   ///   modify it under the terms of the GNU General Public License
+   ///   as published by the Free Software Foundation; either version 2
+   ///   of the License, or (at your option) any later version.
+   ///   
+   ///   This program is distributed in the hope that it will be useful,
+   ///   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   ///   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   ///   GNU General Public License for more details.
+   ///   
+   ///   You should have received a copy of the GNU General Public License
+   ///   along with this program; if not, write to the Free Software
+   ///   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+   /// 
+   ///   The author may be contacted at:
+   ///   ted@astrocomma.com or curtismbeard@gmail.com
+   /// </remarks>
+   /// <history>
+   /// [Curtis_Beard]		06/02/2015	Initial, moved some from Core\Common to here
+   /// </history>
    public sealed class Utils
    {
       /// <summary>
